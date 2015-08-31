7d9f1193292c1fe6dbdefae05e8f6643c1e87078
DarekDan <dydek@hotmail.com>
2015-08-31 18:32:08 -0500

    Added UnitTest project for Dapper NET45

---
+++ UnitTests/Dapper.NET45.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Dapper.NET45.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dapper.NET45.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5101a1f1-928b-4f0c-b129-13096c9bbb1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/Dapper.NET45.Tests/UnitTests.cs
@@ -0,0 +1,247 @@
+﻿#region
+
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+#endregion
+
+namespace Dapper.NET45.Tests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        public static readonly string ConnectionString = "Data Source=.;Initial Catalog=tempdb;Integrated Security=True";
+
+        [Test]
+        public void BasicStringUsageAsync()
+        {
+            using (var connection = Helper.GetOpenConnection())
+            {
+                var query = connection.QueryAsync<string>("select 'abc' as [Value] union all select @txt", new {txt = "def"});
+                var arr = query.Result.ToArray();
+                Assert.IsTrue(arr.SequenceEqual(new[] {"abc", "def"}));
+            }
+        }
+
+        [Test]
+        public void BasicStringUsageAsyncNonBuffered()
+        {
+            using (var connection = Helper.GetOpenConnection())
+            {
+                var query =
+                    connection.QueryAsync<string>(new CommandDefinition("select 'abc' as [Value] union all select @txt", new {txt = "def"},
+                        flags: CommandFlags.None));
+                var arr = query.Result.ToArray();
+                Assert.IsTrue(arr.SequenceEqual(new[] {"abc", "def"}));
+            }
+        }
+
+        [Test]
+        public void BasicStringUsageClosedAsync()
+        {
+            using (var connection = Helper.GetClosedConnection())
+            {
+                var query = connection.QueryAsync<string>("select 'abc' as [Value] union all select @txt", new {txt = "def"});
+                var arr = query.Result.ToArray();
+                Assert.IsTrue(arr.SequenceEqual(new[] {"abc", "def"}));
+            }
+        }
+
+        [Test]
+        public void ClassWithStringUsageAsync()
+        {
+            using (var connection = Helper.GetOpenConnection())
+            {
+                var query = connection.QueryAsync<BasicType>("select 'abc' as [Value] union all select @txt", new {txt = "def"});
+                var arr = query.Result.ToArray();
+                Assert.IsTrue(arr.Select(x => x.Value).SequenceEqual(new[] {"abc", "def"}));
+            }
+        }
+
+        [Test]
+        public void ExecuteAsync()
+        {
+            using (var connection = Helper.GetOpenConnection())
+            {
+                var query = connection.ExecuteAsync("declare @foo table(id int not null); insert @foo values(@id);", new {id = 1});
+                var val = query.Result;
+                Assert.AreEqual(val, 1);
+            }
+        }
+
+        [Test]
+        public void ExecuteClosedConnAsync()
+        {
+            using (var connection = Helper.GetClosedConnection())
+            {
+                var query = connection.ExecuteAsync("declare @foo table(id int not null); insert @foo values(@id);", new {id = 1});
+                var val = query.Result;
+                Assert.AreEqual(val, 1);
+            }
+        }
+
+
+        [Test]
+        public void LongOperationWithCancellation()
+        {
+            using (var connection = Helper.GetClosedConnection())
+            {
+                var cancel = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+                var task = connection.QueryAsync<int>(new CommandDefinition("waitfor delay '00:00:10';select 1", cancellationToken: cancel.Token));
+                try
+                {
+                    if (!task.Wait(TimeSpan.FromSeconds(7)))
+                    {
+                        throw new TimeoutException(); // should have cancelled
+                    }
+                }
+                catch (AggregateException agg)
+                {
+                    Assert.IsTrue(agg.InnerException is SqlException);
+                }
+            }
+        }
+
+        [Test]
+        public void MultiClosedConnAsync()
+        {
+            using (var conn = Helper.GetClosedConnection())
+            {
+                using (var multi = conn.QueryMultipleAsync("select 1; select 2").Result)
+                {
+                    Assert.AreEqual(multi.ReadAsync<int>().Result.Single(), 1);
+                    Assert.AreEqual(multi.ReadAsync<int>().Result.Single(), 2);
+                }
+            }
+        }
+
+        [Test]
+        public void MultiMapArbitraryWithSplitAsync()
+        {
+            var sql = @"select 1 as id, 'abc' as name, 2 as id, 'def' as name";
+            using (var connection = Helper.GetOpenConnection())
+            {
+                var productQuery = connection.QueryAsync(sql, new[] {typeof (Product), typeof (Category)}, objects =>
+                {
+                    var prod = (Product) objects[0];
+                    prod.Category = (Category) objects[1];
+                    return prod;
+                });
+
+                var product = productQuery.Result.First();
+                Assert.AreEqual(product.Id, 1);
+                Assert.AreEqual(product.Name, "abc");
+                Assert.AreEqual(product.Category.Id, 2);
+                Assert.AreEqual(product.Category.Name, "def");
+            }
+        }
+
+        [Test]
+        public void MultiMapWithSplitAsync()
+        {
+            var sql = @"select 1 as id, 'abc' as name, 2 as id, 'def' as name";
+            using (var connection = Helper.GetOpenConnection())
+            {
+                var productQuery = connection.QueryAsync<Product, Category, Product>(sql, (prod, cat) =>
+                {
+                    prod.Category = cat;
+                    return prod;
+                });
+
+                var product = productQuery.Result.First();
+                Assert.AreEqual(product.Id, 1);
+                Assert.AreEqual(product.Name, "abc");
+                Assert.AreEqual(product.Category.Id, 2);
+                Assert.AreEqual(product.Category.Name, "def");
+            }
+        }
+
+        [Test]
+        public void MultiMapWithSplitClosedConnAsync()
+        {
+            var sql = @"select 1 as id, 'abc' as name, 2 as id, 'def' as name";
+            using (var connection = Helper.GetClosedConnection())
+            {
+                var productQuery = connection.QueryAsync<Product, Category, Product>(sql, (prod, cat) =>
+                {
+                    prod.Category = cat;
+                    return prod;
+                });
+
+                var product = productQuery.Result.First();
+                Assert.AreEqual(product.Id, 1);
+                Assert.AreEqual(product.Name, "abc");
+                Assert.AreEqual(product.Category.Id, 2);
+                Assert.AreEqual(product.Category.Name, "def");
+            }
+        }
+
+        [Test]
+        public void MultiOpenConnAsync()
+        {
+            using (var conn = Helper.GetOpenConnection())
+            {
+                using (var multi = conn.QueryMultipleAsync("select 1; select 2").Result)
+                {
+                    Assert.AreEqual(multi.ReadAsync<int>().Result.Single(), 1);
+                    Assert.AreEqual(multi.ReadAsync<int>().Result.Single(), 2);
+                }
+            }
+        }
+
+        [Test]
+        public void QueryDynamicAsync()
+        {
+            using (var connection = Helper.GetClosedConnection())
+            {
+                var row = connection.QueryAsync("select 'abc' as [Value]").Result.Single();
+                string value = row.Value;
+                Assert.AreEqual(value, "abc");
+            }
+        }
+
+        private class BasicType
+        {
+            public string Value { get; set; }
+        }
+
+        private class Category
+        {
+            public string Description { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        private static class Helper
+        {
+            public static SqlConnection GetClosedConnection()
+            {
+                return new SqlConnection(ConnectionString);
+            }
+
+            public static SqlConnection GetOpenConnection(bool mars = false)
+            {
+                var cs = ConnectionString;
+                if (mars)
+                {
+                    var scsb = new SqlConnectionStringBuilder(cs);
+                    scsb.MultipleActiveResultSets = true;
+                    cs = scsb.ConnectionString;
+                }
+                var connection = new SqlConnection(cs);
+                connection.Open();
+                return connection;
+            }
+        }
+
+        private class Product
+        {
+            public Category Category { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
